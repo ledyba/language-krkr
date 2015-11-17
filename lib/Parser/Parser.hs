71f9e29199fd06170d4721d74ddf10b73a96f960
Kaede Fujisaki <kaede@hexe.net>
2015-11-17 16:11:00 +0900

    refactor

--- lib/Parser/Parser.hs
+++ lib/Parser/Parser.hs
@@ -29,21 +29,6 @@ expr = choice [numLit, strLit, identifer]
 -- Terms
 --------------------------------------------------------------------------------
 
-comment :: Parser ()
-comment = do
-    spaces
-    choice [oneline, multline]
-    spaces
-  where
-    oneline = do
-      try $ void $ string "//"
-      void $ manyTill anyChar (try $ choice [void newline, eof])
-      return ()
-    multline = do
-      try $ void $ string "/*"
-      void $ manyTill anyChar (try (string "*/"))
-      return ()
-
 term :: Parser Expr
 term = do
   tjspace
@@ -66,9 +51,6 @@ term = do
 -- Literals
 --------------------------------------------------------------------------------
 
-tjspace :: Parser ()
-tjspace = optional $ skipMany1 $ choice [void space, comment]
-
 identifer :: Parser Expr
 identifer = do
     f <- choice [letter, char '_']
@@ -77,24 +59,30 @@ identifer = do
 
 arrayLit :: Parser Expr
 arrayLit = do
-  void $ char '['
-  lits <- expr `sepEndBy` char ','
-  void $ char ']'
-  return $ Array lits
-
-dictItem :: Parser (String, Expr)
-dictItem = do
-  key <- stringLit
-  void $ string "=>"
-  value <- expr
-  return (key, value)
+    void $ char '['
+    tjspace
+    lits <- expr `sepEndBy` char ','
+    tjspace
+    void $ char ']'
+    return $ Array lits
 
 dictLit :: Parser Expr
 dictLit = do
-  void $ string "%["
-  lits <- dictItem `sepEndBy` char ','
-  void $ char ']'
-  return $ Dict lits
+    void $ string "%["
+    tjspace
+    lits <- dictItem `sepEndBy` char ','
+    tjspace
+    void $ char ']'
+    return $ Dict lits
+  where
+    dictItem :: Parser (String, Expr)
+    dictItem = do
+      key <- stringLit
+      tjspace
+      void $ string "=>"
+      tjspace
+      value <- expr
+      return (key, value)
 
 uniLit :: Parser Char
 uniLit = do
@@ -102,52 +90,6 @@ uniLit = do
     case readHex str of
       [] -> fail ("Could not read as hex: 0x" ++ str)
       (s,_) : _ -> return $ chr s
-
-singleStringLitEsc :: Parser Char
-singleStringLitEsc = char '\\' >> choice
-            [ char '\\' >> return '\\',
-             char '\'' >> return '\'',
-             char 'a' >> return (chr 0x7),
-             char 'b' >> return (chr 0x8),
-             char 'f' >> return (chr 0xc),
-             char 'n' >> return '\n',
-             char 'r' >> return '\r',
-             char 't' >> return '\t',
-             char 'v' >> return (chr 0xb),
-             oneOf "xX" >> uniLit
-            ]
-
-singleStringLit :: Parser String
-singleStringLit = do
-    void $ char '\''
-    str <- many $ choice [singleStringLitEsc, P.noneOf "\'"]
-    void $ char '\''
-    return str
-
-
-doubleStringLitEsc :: Parser Char
-doubleStringLitEsc = char '\\' >> choice
-            [ char '\\' >> return '\\',
-             char '\"' >> return '"',
-             char 'a' >> return (chr 0x7),
-             char 'b' >> return (chr 0x8),
-             char 'f' >> return (chr 0xc),
-             char 'n' >> return '\n',
-             char 'r' >> return '\r',
-             char 't' >> return '\t',
-             char 'v' >> return (chr 0xb),
-             oneOf "xX" >> uniLit
-            ]
-
-doubleStringLit :: Parser String
-doubleStringLit = do
-    void $ char '\"'
-    str <- many $ choice [doubleStringLitEsc, P.noneOf "\""]
-    void $ char '\"'
-    return str
-
-stringLit :: Parser String
-stringLit = choice [doubleStringLit, singleStringLit]
 
 zeroLit :: Parser Expr
 zeroLit = char '0' >> return (Int 0)
@@ -184,19 +126,6 @@ binLit = do
     [] -> fail ("Could not read as binary: 0" ++ r : n)
     (s,_) : _ -> return (Int s)
 
-decFloatLit :: Parser Double
-decFloatLit = do
-    void $ char '.'
-    ds <- many1 digit
-    return $ read $ '0':'.':ds
-
-decExpLit :: Parser Integer
-decExpLit = do
-    void $ oneOf "eE"
-    s <- option 1 (char '-' >> return (-1))
-    ds <- many1 digit
-    return $ s * read ds
-
 decLit :: Parser Expr
 decLit = do
     d <- decIntLit
@@ -207,9 +136,78 @@ decLit = do
       (Just f1, Nothing) -> Real $ fromInteger d + f1
       (Nothing, Just e1) -> Int $ fromInteger (d * (10 ^ e1))
       (Just f1, Just e1) -> Real $ (fromInteger d + f1) * fromInteger (10 ^ e1)
+    where
+      decFloatLit :: Parser Double
+      decFloatLit = do
+          void $ char '.'
+          ds <- many1 digit
+          return $ read $ '0':'.':ds
+
+      decExpLit :: Parser Integer
+      decExpLit = do
+          void $ oneOf "eE"
+          s <- option 1 (char '-' >> return (-1))
+          ds <- many1 digit
+          return $ s * read ds
 
 numLit :: Parser Expr
 numLit = choice [decLit, octLit, hexLit, binLit, zeroLit]
 
 strLit :: Parser Expr
-strLit = Str <$> choice [singleStringLit, doubleStringLit]
+strLit = Str <$> stringLit
+
+stringLit :: Parser String
+stringLit = choice [singleStringLit, doubleStringLit]
+  where
+    singleStringLitEsc :: Parser Char
+    singleStringLitEsc = char '\\' >> choice
+                [ char '\\' >> return '\\',
+                 char '\'' >> return '\'',
+                 char 'a' >> return (chr 0x7),
+                 char 'b' >> return (chr 0x8),
+                 char 'f' >> return (chr 0xc),
+                 char 'n' >> return '\n',
+                 char 'r' >> return '\r',
+                 char 't' >> return '\t',
+                 char 'v' >> return (chr 0xb),
+                 oneOf "xX" >> uniLit
+                ]
+    singleStringLit :: Parser String
+    singleStringLit = do
+        void $ char '\''
+        str <- many $ choice [singleStringLitEsc, P.noneOf "\'"]
+        void $ char '\''
+        return str
+    doubleStringLitEsc :: Parser Char
+    doubleStringLitEsc = char '\\' >> choice
+                [ char '\\' >> return '\\',
+                 char '\"' >> return '"',
+                 char 'a' >> return (chr 0x7),
+                 char 'b' >> return (chr 0x8),
+                 char 'f' >> return (chr 0xc),
+                 char 'n' >> return '\n',
+                 char 'r' >> return '\r',
+                 char 't' >> return '\t',
+                 char 'v' >> return (chr 0xb),
+                 oneOf "xX" >> uniLit
+                ]
+    doubleStringLit :: Parser String
+    doubleStringLit = do
+        void $ char '\"'
+        str <- many $ choice [doubleStringLitEsc, P.noneOf "\""]
+        void $ char '\"'
+        return str
+
+--------------------------------------------------------------------------------
+-- WhiteSpaces
+--------------------------------------------------------------------------------
+
+tjspace :: Parser ()
+tjspace = skipMany (void space <|> comment)
+    where comment = choice [oneline, multline]
+          oneline = do
+            void $ try $ string "//"
+            void $ manyTill anyChar (try $ choice [void newline, eof])
+          multline = do
+            void $ try $ string "/*"
+            void $ manyTill anyChar (try (string "*/"))
